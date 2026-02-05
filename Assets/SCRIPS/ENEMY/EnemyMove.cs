7a6912c0c6149c276a5d243c52277ef2515e77a9
rodriguezgutierreznichel-rgb <rodriguezgutierreznichel@gmail.com>
2026-02-05 12:12:56 +0000

    Mejoras en el sistema de disparo del player

--- Assets/SCRIPS/ENEMY/EnemyMove.cs
+++ Assets/SCRIPS/ENEMY/EnemyMove.cs
@@ -1,36 +1,36 @@
+using Unity.Cinemachine;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class EnemyMove : MonoBehaviour
 {
-    [SerializeField]
-    NavMeshAgent agent;
-    [SerializeField]
-    Transform targetPosition;
-    [SerializeField]
-    Animator animator;
+    [SerializeField] Transform player;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        animator = GetComponent<Animator>();
-        if (animator == null)
-        {
-            Debug.LogError("No tienes un animator en " + gameObject.name, gameObject);
-        }
-
-        agent.SetDestination(targetPosition.position);
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        animator.SetFloat("Walking", agent.velocity.magnitude);
+       
 
     }
 
-    public void Caminar()
+    private void OnTriggerEnter(Collider other)
     {
+        Vector3 direccion = player.position - transform.position;
 
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit hit;
+
+        if (other.CompareTag("Player") && Physics.Raycast(ray, out hit) && (hit.collider. CompareTag("Player")))
+        {
+            Debug.Log("Te vi");
+            Debug.DrawRay((transform.position), direccion, Color.red);
+        }
     }
 }
